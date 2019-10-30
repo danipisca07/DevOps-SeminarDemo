6ac5119065ef107095d62194531b4038b82f888f
danipisca07 <danielepiscaglia@libero.it>
2019-10-31 00:21:51 +0100

    Environment variable test

--- src/test/java/basicTests/SimpleTest.java
+++ src/test/java/basicTests/SimpleTest.java
@@ -2,11 +2,14 @@ package basicTests;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class SimpleTest {
 
     @Test
     public void test1() {
         System.out.println("Test 1 works");
+        assertEquals("19",System.getenv("TEST_ENV"));
     }
 
 
